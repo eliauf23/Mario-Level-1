b8684581e027db7f66516aebea4c46774a12a057
saml1 <72950040+saml1@users.noreply.github.com>
2023-04-18 18:48:09 -0400

    added some more tests for tools.py

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -40,14 +40,14 @@ class testTools(TestCase):
         self.assertEqual(self.control.state_dict, state_dict)
         self.assertEqual(self.control.state, state_dict[c.MAIN_MENU])
 
-    def test_update_quit(self): # note state is a class
+    def test_update_quit(self):
         self.control.state = Mock(quit=True)
         with patch.object(self.control.state, "update") as mock_update:
             self.control.update()
             mock_update.assert_called()
             self.assertEqual(self.control.done, True)
 
-    def test_update_done(self): # note state is a class
+    def test_update_done(self):
         self.control.state = Mock(quit=False, done=True)
         with patch.object(self.control, "flip_state") as mock_flip_state:
             with patch.object(self.control.state, "update") as mock_update:
@@ -56,12 +56,51 @@ class testTools(TestCase):
                 mock_update.assert_called()
 
     def test_flip_state(self):
-        pass
-        # self.control.state = Mock(next=c.MAIN_MENU)
-        # self.control.state_name = c.GAME_OVER
-        # self.control.state_dict[c.MAIN_MENU] = c.LEVEL_STATE
-        # self.control.flip_state()
-        # with patch.object(self.control.state, "startup") as mock_startup:
-        #     # pass
-        #     self.control.flip_state()
-            # mock_startup.assert_called()
+        self.control.state = _State()
+        self.control.state.next = c.GAME_OVER
+        self.control.state_dict[c.GAME_OVER] = _State()
+        with patch.object(_State, "startup") as mock_startup:
+            with patch.object(_State, "cleanup") as mock_cleanup:
+                self.control.flip_state()
+                mock_startup.assert_called()
+                mock_cleanup.assert_called()
+
+    def test_event_loop_quit(self):
+        self.control.state = _State()
+        return_quit = MagicMock(return_value=[pg.event.Event(pg.QUIT)])
+        with patch.object(pg.event, "get", new=return_quit):
+            with patch.object(_State, "get_event") as mock_get_event:
+                self.control.event_loop()
+                mock_get_event.assert_called()
+
+    def test_event_loop_keydown(self):
+        self.control.state = _State()
+        return_keydown = MagicMock(return_value=[pg.event.Event(pg.KEYDOWN, {'key': pg.K_SPACE})])
+        with patch.object(self.control, "toggle_show_fps") as mock_toggle_show_fps:
+            with patch.object(pg.event, "get", new=return_keydown):
+                with patch.object(_State, "get_event") as mock_get_event:
+                    self.control.event_loop()
+                    mock_get_event.assert_called()
+                    mock_toggle_show_fps.assert_called()
+                    self.assertEqual(self.control.keys, pg.key.get_pressed())
+
+    def test_event_loop_keyup(self):
+        self.control.state = _State()
+        return_keyup = MagicMock(return_value=[pg.event.Event(pg.KEYUP, {'key': pg.K_SPACE})])
+        with patch.object(pg.event, "get", new=return_keyup):
+            with patch.object(_State, "get_event") as mock_get_event:
+                self.control.event_loop()
+                mock_get_event.assert_called()
+                self.assertEqual(self.control.keys, pg.key.get_pressed())
+
+    def test_toggle_show_fps(self):
+        self.control.show_fps = True
+        with patch.object(pg.display, "set_caption") as mock_set_caption:
+            self.control.toggle_show_fps(pg.K_F5)
+            self.assertFalse(self.control.show_fps)
+            mock_set_caption.assert_called()
+
+
+
+
+
